05d75a92672038268c5505a517e97d56dc092cb6
Tomoko <haisaitomoco@gmail.com>
2021-02-08 11:13:51 +0200

    Tilanne osa 2.Display-a-list (https://github.com/Tomoko-hub/angular-tour-of-heroes).

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id:11, name: 'Dr Nice' },
-  { id:12, name: 'Narco' },
+  { id:11, name: 'Yawara' },
+  { id:12, name: 'Naruto' },
   { id:13, name: 'Wiseman' },
   { id:14, name: 'Markku' },
   { id:15, name: 'Inoki' },
   { id:16, name: 'Mr Bomb' },
-  { id:17, name: 'Topi' },
-  { id:18, name: 'Taavi' },
+  { id:17, name: 'Mari' },
+  { id:18, name: 'Susanna' },
   { id:19, name: 'Vincent' },
-  { id:20, name: 'Rene' }
+  { id:20, name: 'Ray' }
 ]
